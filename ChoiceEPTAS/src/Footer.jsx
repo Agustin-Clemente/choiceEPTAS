f2e6511b747d54723fdaa39c221bd29ade5cf991
Agush <a_clemente@live.com.ar>
2025-06-07 17:52:50 -0300

    Footer agregado

---
+++ ChoiceEPTAS/src/Footer.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <footer class="bg-purple-950 text-white text-center py-10 shadow-xl ">
+            <p class="text-2xl italic text-purple-300 text-center">&copy; 2025 Clemente Agustín. Todos los derechos reservados.</p>
+        </footer>
+    )
+}
+
+export default Footer
